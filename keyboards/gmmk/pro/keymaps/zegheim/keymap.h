b63950ef493f98944096a3a834c2661903080ec2
Justin Arista Kasin <justin+github@aristakas.in>
2021-06-13 11:12:02 +0100

    encoder_update_user

--- keyboards/gmmk/pro/keymaps/zegheim/keymap.h
+++ keyboards/gmmk/pro/keymaps/zegheim/keymap.h
@@ -29,9 +29,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
 
     if (clockwise) {
-      tap_code(KC_TAB);
+        tap_code(KC_TAB);
     } else {
-      tap_code16(S(KC_TAB));
+        tap_code16(S(KC_TAB));
     }
 
     return true;
